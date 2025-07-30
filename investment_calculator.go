f451558739c3d365a508ea8fc7e5a199ff895f88
Cesar Casares <cacjmuse@gmamil.com>
2025-07-29 22:51:55 -0500

    feat: An Alternative Return Value Syntax

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,28 +5,32 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
 	//pointer important in go
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var years float64
-	expectedReturnRate := 5.5
+	var expectedReturnRate = 5.5
 
 	fmt.Println("|||||||||||||||||||||||||||||||||||||||||")
-	fmt.Print("Investment Amount: ")
-
+	// fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
 	fmt.Println("|||||||||||||||||||||||||||||||||||||||||")
-	fmt.Print("Expected Return Rate: ")
+	// fmt.Print("Expected Return Rate: ")
+	outputText("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
 	fmt.Println("|||||||||||||||||||||||||||||||||||||||||")
-	fmt.Print("Years: ")
+	// fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValue(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
 	formattedRFV := fmt.Sprintf("Future Value (adjusted for inflation): %.1f", futureRealValue)
@@ -37,4 +41,15 @@ func main() {
 	// fmt.Println("Future Value (adjusted for inflation): ", futureRealValue)
 
 	fmt.Print(formattedFV, formattedRFV)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValue(investmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
+	//return
 }
