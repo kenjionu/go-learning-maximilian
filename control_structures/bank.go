dfd2610088ecf5323b3b37e0a80c76a63acf5d04
Cesar Casares <cacjmuse@gmamil.com>
2025-08-07 23:40:11 -0500

    feat(section 2: go essentails): Writing to Files

--- control_structures/bank.go
+++ control_structures/bank.go
@@ -8,61 +8,15 @@ import (
 	"os"
 )
 
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile("balance.txt", []byte(balanceText), 0644)
+}
+
 func main() {
 	var accountBalance float64 = 1000.00
-
-	for i := 0; i < 2; i++ {
-		fmt.Println("What would you like to do?")
-		fmt.Println("1. Check Balance")
-		fmt.Println("2. Deposit")
-		fmt.Println("3. Withdraw")
-		fmt.Println("4. Exit")
-
-		var choice int
-		fmt.Print("Your choice: ")
-		fmt.Scan(&choice)
-
-		// wantsCheckBalance := choice == 1
-
-		if choice == 1 {
-			fmt.Println("Your balance  is: ", accountBalance)
-		} else if choice == 2 {
-			fmt.Println("Your deposit")
-			var depositAmount float64
-			fmt.Scan(&depositAmount)
-
-			if depositAmount <= 0 {
-				fmt.Println("Deposit amount must be greater than 0.")
-				return
-			}
-			accountBalance = deposit(accountBalance, depositAmount)
-			fmt.Printf("Balance updated! New amount: %.2f\n", accountBalance)
-		} else if choice == 3 {
-			fmt.Println("Your withdraw")
-			var withdrawalAmount float64
-			fmt.Scan(&withdrawalAmount)
-
-			if withdrawalAmount <= 0 {
-				fmt.Println("Withdrawal amount must be greater than 0.")
-				return
-			}
-
-			if withdrawalAmount > accountBalance {
-				fmt.Println("Insufficient funds: you cannot withdraw more than your current balance.")
-				return
-			}
-
-			accountBalance += withdrawalAmount
-			fmt.Println("Your new balance is: ", accountBalance)
-		} else {
-			fmt.Println("Exiting the bank. Goodbye!")
-			return
-		}
-
-		fmt.Println("Your choice is: ", choice)
-	}
 	fmt.Println("Welcome to go Bank")
-	img, err := loadIamge("bank4.png")
+	img, err := loadImage("bank4.png")
 	if err != nil {
 		panic((err))
 	}
@@ -76,28 +30,65 @@ func main() {
 		}
 		fmt.Println()
 	}
+	for {
+		fmt.Println("What would you like to do?")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit")
+		fmt.Println("3. Withdraw")
+		fmt.Println("4. Exit")
 
-}
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
 
-func checkBalance(balance float64) {
-	fmt.Scan()
-	fmt.Printf("Your current balance is: %.2f\n", balance)
-}
-func deposit(balance float64, amount float64) float64 {
-	fmt.Printf("Depositiing ^%.2f\n", amount)
-	return balance + amount
-}
+		// wantsCheckBalance := choice == 1
 
-func withdraw(balance, amount float64) (float64, error) {
-	if amount > balance {
-		return balance, fmt.Errorf("Insufficient funds: you cannot withdraw %.2f from a balance of %.2f", amount, balance)
-	} else {
-		fmt.Printf("Withdrawing %.2f\n", amount)
-		return balance - amount, nil
+		switch choice {
+		case 1:
+			fmt.Println("Your balance  is: ", accountBalance)
+		case 2:
+			fmt.Println("Your deposit")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Deposit amount must be greater than 0.")
+				continue
+			}
+
+			accountBalance += depositAmount
+			fmt.Printf("Balance updated! New amount: %.2f\n", accountBalance)
+			writeBalanceToFile(accountBalance)
+		case 3:
+			fmt.Println("Your withdraw")
+			var withdrawalAmount float64
+			fmt.Scan(&withdrawalAmount)
+
+			if withdrawalAmount <= 0 {
+				fmt.Println("Withdrawal amount must be greater than 0.")
+				continue
+			}
+
+			if withdrawalAmount > accountBalance {
+				fmt.Println("Insufficient funds: you cannot withdraw more than your current balance.")
+				continue
+			}
+
+			accountBalance -= withdrawalAmount
+
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+			writeBalanceToFile(accountBalance)
+
+		default:
+			fmt.Println("Exiting the bank. Goodbye!")
+			fmt.Println("Thank you for choosing go Bank!")
+			return
+		}
 	}
+
 }
 
-func loadIamge(filePath string) (image.Image, error) {
+func loadImage(filePath string) (image.Image, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open image file: %w", err)
