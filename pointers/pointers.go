6efeb7c00154e7c394a04c605d0e35d8f91731bf
Cesar Casares <cacjmuse@gmamil.com>
2025-09-12 20:50:29 -0500

    feat(section 4): Writing Code Without Pointers

---
+++ pointers/pointers.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//go garbage collector in this case when use the function the variable age is copied to the function stack frame
+// and when the function ends the stack frame is deleted and the variable age is lost
+// if we want to modify the variable age in the function we need to use pointers
+
+func main() {
+	age := 32 /// regular variable
+
+	agePointer := &age // pointer to the variable age
+	fmt.Println("Age Pointer: ", agePointer)
+	fmt.Println("Age Pointer Value: ", *agePointer) // dereference the pointer to get the value
+
+	fmt.Println("Age: ", age)
+	adultYears := getAdultYears(age)
+	fmt.Println(adultYears)
+}
+
+func getAdultYears(age int) int {
+	return age - 18
+}
