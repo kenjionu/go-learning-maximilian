0a171f6b2d2ef3f19ca5aa244629bd4187af37f5
Cesar Casares <cacjmuse@gmamil.com>
2025-10-20 21:13:40 -0500

    feat(section 7): Section 7: Managing Related Data with Arrays, Slices & Maps

---
+++ section_7_managing_related_data_with_arryas_slices_maps/go.mod
@@ -0,0 +1,3 @@
+module example.gom/section7
+
+go 1.24.5
---
+++ section_7_managing_related_data_with_arryas_slices_maps/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	title string
+	id    string
+	price float64
+}
+
+func main() {
+	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	fmt.Println(prices)
+}
