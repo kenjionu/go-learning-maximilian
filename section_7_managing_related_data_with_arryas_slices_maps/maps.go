2ead70e7fa5d5e985089366c266e46302e5eeb32
Cesar Casares <cacjmuse@gmamil.com>
2026-01-24 13:19:31 -0500

    feat(section 7): Mutating Maps

--- section_7_managing_related_data_with_arryas_slices_maps/maps.go
+++ section_7_managing_related_data_with_arryas_slices_maps/maps.go
@@ -8,4 +8,20 @@ func main() {
 		"Amazon Web Services": "https://aws.amazon.com",
 	}
 	fmt.Println(websites)
+
+	//advantage you have constant time complexity O(1) to access any value by its key
+	fmt.Println(websites["Amazon Web Services"])
+
+	//with maps you can add new key-value pairs at any time
+	websites["Linkedin"] = "https://linkedin.com"
+	fmt.Println(websites)
+
+	//overwrite existing value
+	websites["Google"] = "https://google.co.uk"
+	fmt.Println(websites)
+
+	//delete key-value pair
+	delete(websites, "Linkedin")
+	fmt.Println(websites)
+
 }
