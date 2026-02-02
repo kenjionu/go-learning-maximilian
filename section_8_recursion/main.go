844fbca4255280cad568ab5f317ca8e90659e3a6
cesar.casares <cesarantonio.casaresjaramillo.contractor@gdevtools.com>
2026-02-02 18:11:25 -0500

    feat(section 8): Splitting Slices Into Parameter Values

--- section_8_recursion/main.go
+++ section_8_recursion/main.go
@@ -2,12 +2,15 @@ package main
 import "fmt"
 
 func main () {
-	sum := 	sumup(1, 10, 15)
+	numbers := []int{1, 10, 15}
+	sum := 	sumup(1, 10, 15, 40, -5)
+	anotherSum := sumup(1, numbers...)
 
 	fmt.Println(sum)
+	fmt.Println(anotherSum)
 }
 
-func sumup(startingValue ...int, numbers ...int) int {
+func sumup(startingValue int, numbers ...int) int {
 	sum := 0
 
 	for _, val := range numbers {
