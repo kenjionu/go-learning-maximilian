38aadd00f78e2fad6e2b93317c244c3ef23692bb
cesar.casares <cesarantonio.casaresjaramillo.contractor@gdevtools.com>
2026-02-06 18:38:20 -0500

    feat(section 9): Working With The File Data

--- section_9_practice_price_calculator/prices/prices.go
+++ section_9_practice_price_calculator/prices/prices.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type taxIncludedPriceJob struct {
@@ -12,7 +13,7 @@ type taxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]float64
 }
 
-func (job taxIncludedPriceJob) LoadData() {
+func (job *taxIncludedPriceJob) LoadData() {
 	// Load data logic here (if needed)
 	file, err := os.Open("prices.txt")
 
@@ -37,13 +38,33 @@ func (job taxIncludedPriceJob) LoadData() {
 		file.Close()
 		return
 	}
+
+	prices := make([]float64, len(lines))
+
+	for lineIndex, line := range lines {
+		floatPrice, err := strconv.ParseFloat(line, 64)
+
+		if err != nil {
+			fmt.Println("Converting price to float failed.")
+			fmt.Println(err)
+			file.Close()
+			return
+		}
+
+		prices[lineIndex] = floatPrice
+	}
+
+	job.InputPrices = prices
 }
 
-func (job taxIncludedPriceJob) Process() {
-	result := make(map[string]float64)
+func (job *taxIncludedPriceJob) Process() {
+	job.LoadData()
+
+	result := make(map[string]string)
 
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
 	fmt.Println(result)
