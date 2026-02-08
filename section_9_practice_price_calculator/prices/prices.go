01189af111252063f3735cf79e927747a7a9686e
= <=>
2026-02-07 23:05:41 -0500

    feat(section 9):  Adding & Using Struct Tags

--- section_9_practice_price_calculator/prices/prices.go
+++ section_9_practice_price_calculator/prices/prices.go
@@ -8,10 +8,10 @@ import (
 )
 
 type taxIncludedPriceJob struct {
-	IOManager         filemanager.FileManager
-	TaxRate           float64
-	InputPrices       []float64
-	TaxIncludedPrices map[string]string
+	IOManager         filemanager.FileManager `json:"-"`
+	TaxRate           float64                 `json:"tax_rate"`
+	InputPrices       []float64               `json:"input_prices"`
+	TaxIncludedPrices map[string]string       `json:"tax_included_prices"`
 }
 
 func (job *taxIncludedPriceJob) LoadData() {
