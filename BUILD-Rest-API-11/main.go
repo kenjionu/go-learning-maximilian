0275ce4c051698f03be58176aa734aa01a214e71
cesar.casares <cesarantonio.casaresjaramillo.contractor@gdevtools.com>
2026-02-10 18:17:28 -0500

    feat(section 11): Installing the Gin Framework

---
+++ BUILD-Rest-API-11/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+// test
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listens on 0.0.0.0:8080 by default
+}
