fcdab44b592dca6f53d731bb02738db83089458b
= <=>
2026-02-16 21:45:40 -0500

    feat(section 11): Setting Up A First Route & Handling a First Request

--- BUILD-Rest-API-11/main.go
+++ BUILD-Rest-API-11/main.go
@@ -1,14 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // test
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run() // listens on 0.0.0.0:8080 by default
+	server := gin.Default()
+	server.GET("/events", getEvents)
+	server.Run(":8080") // listens on 0.0.0.0:8080 by default
+}
+
+func getEvents(Context *gin.Context) {
+	Context.JSON(http.StatusOK, gin.H{"message": "Hello"})
 }
