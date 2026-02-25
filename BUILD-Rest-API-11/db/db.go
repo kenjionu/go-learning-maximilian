9d17526a51aa0216645da235a6128dacde8676f2
cesar.casares <cesarantonio.casaresjaramillo.contractor@gdevtools.com>
2026-02-25 17:55:06 -0500

    feat(section 11):  Creating A SQL Database Table

--- BUILD-Rest-API-11/db/db.go
+++ BUILD-Rest-API-11/db/db.go
@@ -3,12 +3,14 @@ package db
 import (
 	"database/sql"
 
+	"example.com/BUILD-Rest-API-11/db"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
 
 func InitDB() (*sql.DB, error) {
+	db.InitDB()
 	// later, connect to a real database
 	DB, err := sql.Open("splite3", "api.db")
 
@@ -18,4 +20,23 @@ func InitDB() (*sql.DB, error) {
 
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
+}
+
+func createTables() {
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		date_time DATETIME NOT NULL,
+		user_id INTEGER NOT NULL
+	);
+	`
+
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		panic("Could not create events table.")
+	}
 }
