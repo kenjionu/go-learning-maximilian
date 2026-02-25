11f39e89729960e70ff67e9f928eba962d15334e
cesar.casares <cesarantonio.casaresjaramillo.contractor@gdevtools.com>
2026-02-25 17:29:01 -0500

    feat(section 11): Testing Requests & Fixing the POST Request Handler

--- BUILD-Rest-API-11/main.go
+++ BUILD-Rest-API-11/main.go
@@ -11,6 +11,7 @@ func main() {
 	server := gin.Default()
 	server.GET("/events", getEvents)
 	server.POST("/events", createEvent)
+
 	server.Run(":8080") // listens on 0.0.0.0:8080 by default
 }
 
@@ -30,5 +31,8 @@ func createEvent(context *gin.Context) {
 
 	event.ID = 1
 	event.UserID = 1
+
+	event.Save()
+
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
