eab19bf4269b4cd304a7cf167a0669b5f761affd
Cesar Casares <cacjmuse@gmamil.com>
2026-03-15 22:22:53 -0500

    feat(section 11): Finishing The JWT Logic

--- BUILD-Rest-API-11/routes/users.go
+++ BUILD-Rest-API-11/routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"example.com/BUILD-Rest-API-11/models"
+	"example.com/BUILD-Rest-API-11/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -44,5 +45,12 @@ func login(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Login Succesful"})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not generate token. Try again later."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login Succesful", "token": token})
 }
